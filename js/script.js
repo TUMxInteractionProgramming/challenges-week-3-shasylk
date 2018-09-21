10d78e2b8133199d67b6457b4668be04a0b226f4
shasylk <43097778+shasylk@users.noreply.github.com>
2018-09-21 16:00:58 -0500

    Week 3
    html, css, java

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-// JavaScript Document
 function switchChannel(channelName)
-{	
-	
-	// switch channel
+{
+'use strict';	
+	// write to console
 
-console.log('App is Alive');
+//console.log('App is Alive');
 	
-$('new-header').html(channelName);
+$('.new-header').html(channelName); 	
 $('flex-item').attr('src="http://ip.lfe.mw.tum.de/sections/star-o.png">');
 	
 }
