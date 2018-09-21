438b53b391882d5c03e978cb0ac786f41f90f3e5
shasylk <43097778+shasylk@users.noreply.github.com>
2018-09-21 02:30:13 -0500

    Challenges Week 3
    HTML, CSS, JAVASCRIPT

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+function switchChannel(channelName)
+{	
+	
+	// switch channel
+
+console.log('App is Alive');
+	
+$('new-header').html(channelName);
+$('flex-item').attr('src="http://ip.lfe.mw.tum.de/sections/star-o.png">');
+	
+}
